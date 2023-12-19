6fc9a4e9afb7aca7d8bc103fce95bd82fa0f51a3
movilidad.edatalia <movilidad@edatalia.com>
2023-12-19 16:17:24 +0100

    Adds shell script to release and deploy on sfa project

---
+++ generate_release_deploy.sh
@@ -0,0 +1,3 @@
+./gradlew :WebFormsSdk:assembleRelease
+cp WebFormsSdk/build/outputs/aar/WebFormsSdk-release.aar /Users/movilidad/Projects/sign-from-app-compose-multiplatform/shared/libs/WebFormsSdk.aar
+
