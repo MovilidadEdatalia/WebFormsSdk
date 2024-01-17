6313f127fcc616f3490d78cb56aa713aaf76ee84
movilidad.edatalia <movilidad@edatalia.com>
2024-01-17 17:16:26 +0100

    0.9.1 Uses 1.6.1 webforms html (fixes data).

--- WebFormsSdk/build.gradle.kts
+++ WebFormsSdk/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         minSdk = 21
-        version = "0.9.0"
+        version = "0.9.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
