37a4d7ec835eeabc1aef2f4621ea7131bddccf10
movilidad.edatalia <movilidad@edatalia.com>
2024-02-08 13:35:13 +0100

    0.9.5 [FIX] Keyboard hides some inputs

--- WebFormsSdk/build.gradle.kts
+++ WebFormsSdk/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         minSdk = 21
-        version = "0.9.4"
+        version = "0.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
